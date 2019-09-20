19e2778dce90b3d6b152d948f18ef3ae2716cb7b
Thales <thales@carteiraglobal.com>
2019-09-20 15:08:47 -0300

    accepts input text

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,8 +1,19 @@
+import { type } from "os"
+
 describe('Input form', () => {
   it('should focus the input on load', () => {
     cy.visit('http://localhost:3030')
 
     cy.focused()
       .should('have.class', 'new-todo')
+  })
+
+  it('accepts input', () => {
+    const typedText = 'Buy Milk!';
+    cy.visit('http://localhost:3030')
+
+    cy.get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText)
   })
 })
